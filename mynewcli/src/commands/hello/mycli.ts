25e90a7dfae0b631fc430a00689d15125b575aba
Jartto <jartto@JarttodeiMac.lan>
2022-06-28 09:11:02 +0800

    refactor: arguments demo

--- mynewcli/src/commands/hello/mycli.ts
+++ mynewcli/src/commands/hello/mycli.ts
@@ -12,14 +12,14 @@ export default class MyCLI extends Command {
     {name: 'secondArg'},
   ]
 
-  async run(){
+  public async run(): Promise<void>{
     // 通过对象的形式获取参数
     // const { args } = this.parse(this.constructor);
     // const { args, flags } = this.parse(this.constructor as Input<typeof BaseCommand.flags>);
     // const {args} = this.parse(this.constructor as any) as any;
-    this.log(`${JSON.stringify(this.parse(MyCLI))}`)
-    // const { args } = this.parse(MyCLI)
-    // console.log(args);
+    // this.log(`${JSON.stringify(this.parse(MyCLI))}`)
+    const { args } = await this.parse(MyCLI)
+    console.log(args);
     // console.log(`running my command with args: ${args.firstArg}, ${args.secondArg}`)
     // 也可以通过数组的形式获取参数
     // const { args } = this.parse(MyCLI)
